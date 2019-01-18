738023b1a9b1ce9e2c1ae86d19023863ebcc5a1f
jlquinn <jlquinn@optonline.net>
2019-01-17 23:29:01 -0500

    Add constructor from full 64 byte array.

--- src/packet.hh
+++ src/packet.hh
@@ -2,6 +2,7 @@
 #define PACKET_HH
 
 #include <cstring>
+#include <array>
 
 // Packet structure
 // byte 0:	command
@@ -11,7 +12,7 @@
 // byte 5:	number of valid data bytes in this packet
 // byte 6,7:	packet crc.  crc-16/ccitt-false with 6,7 set to 0
 // byte 8-63:	data bytes
-
+typedef std::array<unsigned char, 64> pktarray;
 struct packet {
   union {
     unsigned char bytes[64];	// This is a HID packet sent to the keyboard
@@ -24,8 +25,12 @@ struct packet {
     };
   };
 
+  packet() {}
   packet(unsigned char cmd, unsigned char subcmd) : cmd(cmd), sub(subcmd) {
     memset(bytes+2, 0, 62);
+  }
+  packet(const std::array<unsigned char, 64> a) {
+    memcpy(bytes, a.data(), a.size());
   }
   
   void compute_crc();
